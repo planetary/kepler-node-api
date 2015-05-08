daa9c413fe1f465f4705366fe77795d7d1b87e0f
Radu Dan <radu@full-throttle.ro>
2015-05-08 15:09:24 +0300

    Add migrate script that creates default profiles

---
+++ migrations/0001-profiles.js
@@ -0,0 +1,68 @@
+require('coffee-script');
+
+
+var Promise = require('bluebird'),
+    models = require('../models');
+
+
+exports.up = function(next) {
+    models.Profile.create([
+        {
+            'name': 'default'
+        }, {
+            'name': '4k',
+            'width': 3840,
+        }, {
+            'name': '1080p',
+            'width': 1920
+        }, {
+            'name': '720p',
+            'width': 1280
+        }, {
+            'name': 'imac',
+            'width': 2560,
+            'agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A'
+        }, {
+            'name': 'mbp',
+            'width': 1440,
+            'agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A'
+        }, {
+            'name': 'ipad-portrait',
+            'width': 768,
+            'agent': 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25'
+        }, {
+            'name': 'ipad-landscape',
+            'width': 1024,
+            'agent': 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25'
+        }, {
+            'name': 'iphone6-portrait',
+            'width': 375,
+            'agent': 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25'
+        }, {
+            'name': 'iphone6-landscape',
+            'width': 667,
+            'agent': 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25'
+        }, {
+            'name': 'iphone5-portrait',
+            'width': 320,
+            'agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_1_4 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10B350 Safari/8536.25'
+        }, {
+            'name': 'iphone5-landscape',
+            'width': 568,
+            'agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_1_4 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10B350 Safari/8536.25'
+        }, {
+            'name': 'iphone-portrait',
+            'width': 320,
+            'agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3'
+        }, {
+            'name': 'iphone-landscape',
+            'width': 480,
+            'agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3'
+        }
+    ])
+    .then( function() { next(); } );
+};
+
+exports.down = function(next) {
+    next();
+};
